488b6769d74ebeb8c64645516c5afe409d3eb2d5
Christopher Kaster <ikasoki@gmail.com>
2015-07-28 15:06:35 +0200

    add working rss feed

--- app/browser/js/main.js
+++ app/browser/js/main.js
@@ -1,12 +1,75 @@
-var DynamicsApp = angular.module("DynamicsApp", []);
+var app = angular.module("DynamicsApp", []);
+var spawn = require("child_process").spawn;
 
-DynamicsApp.controller("CloseController", function($scope) {
+app.run(function($rootScope) {
+    var path = localStorage.getItem("gw2_path");
+    var valid = false;
+
+    if(path) {
+        // TODO: check if item exists
+    }
+
+    if(!valid) {
+        // TODO: ask user for gw2.exe path
+    }
+
+    path = "C:\\Program Files (x86)\\Guild Wars 2\\Gw2.exe";
+    $rootScope.gw2Executable = path;
+    $rootScope.feedUrl = "https://www.guildwars2.com/en/feed/";
+
+    $rootScope.cachedFeed = {
+        title: localStorage.getItem("cached_news_title"),
+        news: localStorage.getItem("cached_news_content"),
+        link: localStorage.getItem("cached_news_link")
+    };
+})
+
+// override link click behaviour
+app.directive("a", function() {
+    return {
+        restrict: "E",
+        link: function(scope, elem, attrs) {
+
+            elem.on("click", function(e){
+                e.preventDefault();
+
+                var url = attrs.href;
+
+                // is OS X
+                if(process.platform == "darwin") {
+                    spawn("open", [url]);
+                } else { // is Windows
+                    spawn("explorer.exe", [url]);
+                }
+            });
+        }
+   };
+});
+
+app.controller("CloseController", function($scope) {
     $scope.close = function() {
         window.close();
     }
 });
 
-DynamicsApp.controller("AccountsController", function($scope) {
+app.controller("NewsController", ["$scope", "FeedService", function($scope, FeedService) {
+    $scope.feed = $scope.cachedFeed;
+
+    FeedService.parseFeed($scope.feedUrl).then(function(res) {
+        $scope.feeds = res.data.responseData.feed.entries;
+
+        // get the latest feed
+        $scope.feed = $scope.feeds[0];
+        $scope.feed.news = $scope.feed.contentSnippet.replace("Read More", "");
+
+        // cache news
+        localStorage.setItem("cached_news_title", $scope.feed.title);
+        localStorage.setItem("cached_news_content", $scope.feed.news);
+        localStorage.setItem("cached_news_link", $scope.feed.link);
+    });
+}]);
+
+app.controller("AccountsController", function($scope) {
     $scope.accounts = [
         {
             mail: "primary@mail.com",
@@ -19,5 +82,16 @@ DynamicsApp.controller("AccountsController", function($scope) {
     ];
 });
 
-DynamicsApp.controller("ActionsController", function($scope) {
+app.controller("ActionsController", function($scope) {
+    $scope.update = function() {
+        spawn($scope.gw2Executable, ["-image"]);
+    };
 });
+
+app.factory("FeedService", ["$http", function($http) {
+    return {
+        parseFeed: function(url) {
+            return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&amp&num=10&callback=JSON_CALLBACK&q='+ encodeURIComponent(url));
+        }
+    }
+}]);
