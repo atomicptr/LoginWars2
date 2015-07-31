766382b47136246cc73566e2f4990dc78df1e6ff
Christopher Kaster <ikasoki@gmail.com>
2015-07-31 21:37:14 +0200

    move utils into own file

--- app/browser/js/main.js
+++ app/browser/js/main.js
@@ -6,26 +6,6 @@ var ipc = require("ipc");
 
 var ENCRYPTION_TEST_STRING = "Bookah!";
 
-var AES = {};
-
-AES.encrypt = function(string, password) {
-    return CryptoJS.AES.encrypt(string, password).toString();
-};
-
-AES.decrypt = function(string, password) {
-    return CryptoJS.AES.decrypt(string, password).toString(CryptoJS.enc.Utf8);
-}
-
-function query(queryString) {
-    return document.querySelector(queryString);
-}
-
-function decodeHtml(html) {
-    var textArea = document.createElement("textarea");
-    textArea.innerHTML = html;
-    return textArea.value;
-}
-
 app.run(function($rootScope, $localStorage) {
     var path = $localStorage.gw2Path;
 
