4154cbbda8f744cd3a610ecd44c154fcb48411c7
Christopher Kaster <ikasoki@gmail.com>
2015-08-20 14:41:17 +0200

    clean

--- app/browser/js/factories/feeds.js
+++
@@ -1,7 +0,0 @@
-app.factory("FeedService", ["$http", function($http) {
-    return {
-        parseFeed: function(url) {
-            return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&amp&num=10&callback=JSON_CALLBACK&q='+ encodeURIComponent(url));
-        }
-    }
-}]);
--- app/browser/js/utils/egg.js
+++
@@ -1,4 +0,0 @@
-// easter egg :D
-document.onkeyup = Konami.code(function() {
-    // add something funny here...
-});
