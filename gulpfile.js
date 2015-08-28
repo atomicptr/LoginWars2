752b8c4956faf48c3c2cd35ce022eee4962b6c95
Christopher Kaster <ikasoki@gmail.com>
2015-08-28 16:30:45 +0200

    fixed a problem which caused the executable to have a wrong product name, company name, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 var gulp = require("gulp");
-var electron = require("gulp-electron");
 var less = require("gulp-less");
 var run = require("gulp-run-electron");
 var windowsInstaller = require("electron-windows-installer");
+var packager = require("electron-packager");
 var packageJson = require("./app/package.json");
 
 var path = require("path");
 
 var configs = {
-    ELECTRON_VERSION: "v0.30.4",
+    ELECTRON_VERSION: "0.31.1",
     PRODUCT_NAME: packageJson.productName,
     APP_NAME: packageJson.name,
     APP_VERSION: packageJson.version,
     DESCRIPTION: packageJson.description,
-    ICON_URL_PNG: path.resolve(__dirname, "./app/icons/icon.png"),
+    ICON_URL_PNG: path.resolve(__dirname, "app", "icons", "icon.png"),
     AUTHORS: "kasoki"
 };
 
@@ -30,42 +30,50 @@ gulp.task("run", ["build-less"], function() {
 });
 
 gulp.task("package", ["build-less"], function() {
-    var electronSettings = electron({
-        src: "./app",
-        packageJson: packageJson,
-        release: "./build",
-        cache: "./cache",
+    var packagerOptions = {
+        dir: path.resolve(__dirname, "app"),
+        out: path.resolve(__dirname, "build"),
+        name: configs.APP_NAME,
+        platform: "win32",
+        arch: "x64",
         version: configs.ELECTRON_VERSION,
-        packaging: false,
+        icon: path.resolve(__dirname, "app", "icons", "icon.ico"),
+        cache: path.resolve(__dirname, "cache"),
         asar: true,
-        platforms: ["win32-x64"], //, "darwin-x64"], TODO: OS X is not supported atm so there is no point in packaging it by default
-        platformResources: {
-            darwin: {
-                CFBundleDisplayName: configs.APP_NAME,
-                CFBundleIdentifier: configs.APP_NAME,
-                CFBundleName: configs.APP_NAME,
-                CFBundleVersion: configs.APP_VERSION
-            },
-            win: {
-                "version-string": configs.APP_VERSION,
-                "file-version": configs.APP_VERSION,
-                "product-version": configs.APP_VERSION,
-                icon: "./app/icons/icon.ico"
-            }
-        }
-    });
+        overwrite: true,
+        "app-version": configs.APP_VERSION,
 
-    return gulp.src("").pipe(electronSettings).pipe(gulp.dest(""));
+        "version-string": {
+            CompanyName: configs.AUTHORS,
+            FileDescription: configs.DESCRIPTION,
+            ProductVersion: configs.APP_VERSION,
+            ProductName: configs.PRODUCT_NAME,
+            OriginalFilename: configs.APP_NAME,
+            FileVersion: configs.APP_VERSION,
+            ProductVersion: configs.APP_VERSION,
+            InternalName: configs.APP_NAE,
+            LegalCopyright: "Copyright (C) " + configs.AUTHORS + " 2015."
+        }
+    };
+
+    packager(packagerOptions, function(error, appPath) {
+        if(error) {
+            throw error;
+        }
+
+        var exec = require("child_process").exec;
+        exec("explorer.exe " + appPath);
+    });
 });
 
 gulp.task("build-installer", [], function(done) {
     windowsInstaller({
-        appDirectory: path.resolve(__dirname, "build", "v0.30.4", "win32-x64"),
+        appDirectory: path.resolve(__dirname, "build", configs.PRODUCT_NAME + "-win32-x64"),
         authors: configs.AUTHORS,
         productName: configs.PRODUCT_NAME,
         description: configs.DESCRIPTION,
         version: configs.APP_VERSION,
         iconUrl: configs.ICON_URL_PNG,
-        setupIcon: path.resolve(__dirname, "./app/icons/icon.ico")
+        setupIcon: path.resolve(__dirname, "app", "icons", "icon.ico")
     });
 });
