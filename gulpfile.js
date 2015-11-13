c2ceadccd1fd371a9c494d7ddb1ca74b9775e374
Christopher Kaster <ikasoki@gmail.com>
2015-11-13 01:23:09 +0100

    use png instead of gif for install splash

--- gulpfile.js
+++ gulpfile.js
@@ -72,6 +72,6 @@ gulp.task("build-installer", [], function(done) {
         version: configs.APP_VERSION,
         iconUrl: configs.ICON_URL_PNG,
         setupIcon: path.resolve(__dirname, "app", "icons", "icon.ico"),
-        loadingGif: path.resolve(__dirname, "app", "icons", "loading.gif")
+        loadingGif: path.resolve(__dirname, "app", "icons", "loading.png")
     });
 });
